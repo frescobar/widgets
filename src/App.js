3beed002e408b7e950ba1efd61ca6f4bd79c537f
frescobar <frescobar87@gmail.com>
2020-09-02 01:08:57 -0500

    created a Search component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from "react";
-import Accordion from "./components/Accordion";
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 function App() {
-  const items = [
-    {
-      title: "React",
-      content: "React is a JavaScript Library to make beautiful UI ",
-    },
-    {
-      title: "Why Use React?",
-      content:
-        "React is one of the most loved and favorite library among engineers ",
-    },
-    {
-      title: "How do you use React?",
-      content: "You use React by creating components",
-    },
-  ];
+  // const items = [
+  //   {
+  //     title: "React",
+  //     content: "React is a JavaScript Library to make beautiful UI ",
+  //   },
+  //   {
+  //     title: "Why Use React?",
+  //     content:
+  //       "React is one of the most loved and favorite library among engineers ",
+  //   },
+  //   {
+  //     title: "How do you use React?",
+  //     content: "You use React by creating components",
+  //   },
+  // ];
   return (
     <div>
       <h1>Widgets App</h1>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 }
